470defc4fee1c52651f6a86bc61dd796f27d9e21
Din Cheng <din1030@gmail.com>
2013-06-11 20:26:08 +0800

    decorate tab bar with bg pics

---
+++ wakeup/StatisticTabViewController.h
@@ -0,0 +1,13 @@
+//
+//  StatisticTabViewController.h
+//  wakeup
+//
+//  Created by din1030 on 13/6/11.
+//  Copyright (c) 2013年 din1030. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StatisticTabViewController : UITabBarController
+
+@end
